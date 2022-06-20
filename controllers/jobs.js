0f813831734b88f9084d5d225d956d828c8d47ff
Gaeta <jpgaeta.dev@gmail.com>
2022-06-19 21:04:23 -0300

    feat: create job route

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,3 +1,6 @@
+const Jobs = require('../models/Job')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors')
 
 const getAllJobs = async (req, res) => {
   res.send('register')
@@ -8,7 +11,9 @@ const getJob = async (req, res) => {
 }
 
 const createJob = async (req, res) => {
-  res.send('create')
+  req.body.createdBy = req.user.userId
+  const job = await Jobs.create(req.body)
+  res.status(StatusCodes.CREATED).json({job})
 }
 
 const updateJob = async (req, res) => {
